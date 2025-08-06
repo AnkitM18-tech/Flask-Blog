40ea46a267fe76566bbe5208bd2d9c92043d6d0c
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-07 00:14:32 +0530

    announcement route

--- blog/blog/main/routes.py
+++ blog/blog/main/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template, request, Blueprint
-from blog.models import Post
+from blog.models import Post, User
 
 main = Blueprint("main", __name__)
 
@@ -14,3 +14,12 @@ def home():
 @main.route("/about")
 def about():
     return render_template("about.html.j2", title = "About")
+
+@main.route("/announcements")
+def announcements():
+    page = request.args.get("page", 1, type = int)
+    users = User.query.paginate(page= page, per_page = 3)
+    posts = {}
+    for user in users:
+        posts[user] = Post.query.filter_by(author= user).count()
+    return render_template("announcements.html.j2", title = "Announcements", users= users, posts= posts)
